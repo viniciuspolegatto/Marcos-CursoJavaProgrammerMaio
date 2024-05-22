d9aea8fd67ab59f8355f2d53ac41eed9d9aa1c3e
vinicius.polegatto@gmail.com <vinicius.polegatto@gmail.com>
2024-05-21 21:42:50 -0300

    Animal_teste

---
+++ Aula_2/Atleta.java
@@ -0,0 +1,5 @@
+public class Atleta{
+	
+	public String nomeAtleta; // Variavel para armazenar o nome do atleta
+	public int idade; // Variavel para armazenar a idade do atleta
+}
---
+++ Aula_2/Cadastro_de_Animal/Animal.java
@@ -0,0 +1,5 @@
+public class Animal{
+	
+	public String nomeAnimal; // Variavel para armazenar o nome do Animal
+	public int idade; // Variavel para armazenar a idade do Animal
+}
